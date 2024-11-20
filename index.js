971c4f0db4a4fa1053f2672078c40a05940e4665
Manish kumar <manishk77287@gmail.com>
2024-11-20 13:30:09 +0530

    this is complet mostlye

---
+++ index.js
@@ -0,0 +1,60 @@
+const acceskey = "ZnVfwTCkzanh0bOIC42p65ADeBZn5iHKW3niJhcxCp0";
+const search = document.querySelector("#search");
+const searchbtn = document.querySelector(".btn");
+let box = document.querySelector(".data-box");
+const showbtn = document.querySelector(".show-more-btn");
+let inputsearch = "";
+let page = 1;
+
+async function apidata(inputsearch) {
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputsearch}&client_id=${acceskey}`;
+  const respons = await fetch(url);
+  const data = await respons.json();
+  const results = data.results;
+
+  if (page === 1) {
+    box.innerHTML = "";
+  }
+
+  results.forEach((image) => {
+    const div = document.createElement("div");
+    div.classList.add("data");
+    const img = document.createElement("img");
+    img.classList.add("img");
+    img.src = image.urls.small;
+    div.appendChild(img);
+    box.appendChild(div);
+  });
+
+  page++;
+
+  if (results.length < 10) {
+    showbtn.style.display = "none";
+  } else {
+    showbtn.style.display = "block";
+  }
+}
+
+async function getdata() {
+  inputsearch = search.value.trim();
+  apidata(inputsearch);
+}
+
+search.addEventListener("input", (event) => {
+  event.preventDefault();
+  page = 1;
+  getdata();
+});
+
+showbtn.addEventListener("click", () => {
+  getdata();
+});
+
+search.addEventListener("input", (event) => {
+  let searchvalue = search.value.trim();
+  if (searchvalue === "") {
+    apidata("dog");
+  }
+});
+
+apidata("dog");
